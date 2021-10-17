0ca0b97d0e854ddbe1c375a0b3448f8d10b5dcfc
Vyomaan Dave <vydave.711@gmail.com>
2021-10-17 11:45:33 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -61,13 +61,5 @@ $(function () {
     });
 
     /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
-    });
+    // Theme switcher removed - it does not work properly
 });
